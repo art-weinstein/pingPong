39a1690bc8f2eee19d0a8fd9e2c698371762199e
Art Weinstein <artur.weinstein@gmail.com>
2021-09-20 16:23:25 -0700

    count up to an inputted number and replace integers with ping, pong or ping pong
    Co-authored-by: Kevin Funez <kevin.funez315@gmail.com>

--- pingPong.cs
+++ pingPong.cs
@@ -1,2 +1,39 @@
 using System;
 using System.Collections.Generic;
+
+class pingPong
+{
+  static void Main()
+  {
+    Console.WriteLine("Enter a number");
+    string stringNumber = Console.ReadLine();
+    int yourNumber = int.Parse(stringNumber);
+
+    for (int i = 1; i<=yourNumber; i++){
+    if (i % 3 == 0 && i % 5 == 0) {
+      Console.WriteLine("Ping Pong");
+    } else if (i % 5 == 0) {
+      Console.WriteLine("Pong");
+    } else if (i % 3 == 0) {
+      Console.WriteLine("Ping");
+    } else {
+      Console.WriteLine(i);
+    }
+
+
+
+
+
+    // for (int i = 0; i < yourNumber; i++)
+    // {
+    //   if (yourNumber % 3 == 0) {
+    //     Console.WriteLine("Ping");
+    //   } else {
+    //     yourNumber += i;
+    //     Console.WriteLine(yourNumber);
+    //   }
+    // }
+    // Console.WriteLine(i);
+    }
+  }
+}
